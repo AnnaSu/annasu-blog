116346c94ae10552cebf154af6a7cb03d44c5e15
Anna Su <newstory0113@gmail.com>
2019-04-19 19:56:31 +0800

    feat: Add constants, themes

---
+++ src/constants/themes.js
@@ -0,0 +1,13 @@
+import COLOR from './colors';
+
+export const THEME_COLOR = {
+  tech: COLOR.PRIMARY_BLUE,
+  life: COLOR.PRIMARY_PINK,
+  about: COLOR.PRIMARY_GREEN,
+};
+
+export const THEME_LIGHT_COLOR = {
+  tech: COLOR.PRIMARY_LIGHT_BLUE,
+  life: COLOR.PRIMARY_LIGHT_PINK,
+  about: COLOR.PRIMARY_LIGHT_GREEN,
+};
